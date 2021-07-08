b488a2aecf241da669b7eccde5435ef495e5d1f5
adam t lee <adam.lee346@gmail.com>
2021-07-08 12:54:05 -0600

    v2

--- playground.rb
+++ playground.rb
@@ -6,3 +6,24 @@ def say_hello_to_name(name)
 end
 
 say_hello_to_name("Adam")
+
+##########################################
+
+first_name = "Gatsby"
+last_name = "Lee"
+
+puts first_name + " " + last_name
+
+puts "First name is #{first_name}, and last name is #{last_name}"
+# full_name.class - determine the type
+
+###########################################
+
+vble = "first vble"
+
+new_vble = vble
+puts new_vble
+
+vble = "overrode vble"
+puts vble
+puts new_vble
