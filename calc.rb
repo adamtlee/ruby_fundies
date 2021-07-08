b286e115ac24c1b5f45d96cb02ea65abe5f94d16
adam t lee <adam.lee346@gmail.com>
2021-07-08 15:19:26 -0600

    v6 - methods

--- calc.rb
+++ calc.rb
@@ -1,15 +1,36 @@
 puts "Calculator 2.0"
 25.times { print "-" }
-
 puts " "
+
+
+def addition(num_one, num_two)
+    num_one.to_f + num_two.to_f
+end
+
+def subtraction(num_one, num_two)
+    num_one.to_f - num_two.to_f
+end
+
+def multiplication(num_one, num_two)
+    num_one.to_f * num_two.to_f
+end
+
+def division(num_one, num_two)
+    num_one.to_f / num_two.to_f
+end
+
+def mod(num_one, num_two)
+    num_one.to_f % num_two.to_f
+end
+
 puts "Enter the first number: "
 num_one = gets.chomp
 
 puts "Enter the second number: "
 num_two = gets.chomp
 
-puts "the first x second is #{ num_one.to_i * num_two.to_i}"
-puts "the first + second is #{ num_one.to_i + num_two.to_i}"
-puts "the first / second is #{ num_one.to_i / num_two.to_i}"
-puts "the first % second is #{ num_one.to_i % num_two.to_i}"
-puts "the first - second is #{ num_one.to_i - num_two.to_i}"
+puts "the first x second is #{ multiplication(num_one, num_two) }"
+puts "the first + second is #{ addition(num_one, num_two) }"
+puts "the first / second is #{ division(num_one, num_two) }"
+puts "the first % second is #{ mod(num_one, num_two) }"
+puts "the first - second is #{ subtraction(num_one, num_two) }"
