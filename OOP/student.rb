f3b9c4da5ef3719312b5ed8bb471f2f0961cd420
adam <adam.lee346@gmail.com>
2021-07-11 19:22:07 -0600

    added attribute accessors

--- OOP/student.rb
+++ OOP/student.rb
@@ -1,18 +1,18 @@
 class Student
+    
+    # read and write to the attributes
+    attr_accessor :first_name, :last_name, :email
+    # read access only
+    attr_reader :username
+    
     @first_name
     @last_name
     @email
-    @username
+    @username = "TheGreatGatsby"
     @password
 
-    # setter
-    def first_name=(name)
-        @first_name = name
-    end
-
-    # getter
-    def first_name
-        @first_name
+    def set_username
+        @username = "TheGreatGatsby"
     end
 
     def to_s
@@ -24,5 +24,11 @@ gatsby = Student.new
 puts gatsby
 
 gatsby.first_name = "Gatsby"
+gatsby.last_name = "Lee"
+gatsby.email = "gatsby@mail.com"
+gatsby.set_username
 
 puts gatsby.first_name
+puts gatsby.last_name
+puts gatsby.email
+puts gatsby.username
